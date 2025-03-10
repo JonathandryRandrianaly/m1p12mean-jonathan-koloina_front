50f8e28c471ef7df83dc7d2096d8b2fc954e949c
Andry Jonathan Randrianaly <jonathanrandrianaly@gmail.com>
2025-03-10 10:21:56 +0300

    creation component parametre user

---
+++ src/app/components/parametre/user-parametre/user-parametre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-parametre',
+  imports: [],
+  templateUrl: './user-parametre.component.html',
+  styleUrl: './user-parametre.component.css'
+})
+export class UserParametreComponent {
+
+}
