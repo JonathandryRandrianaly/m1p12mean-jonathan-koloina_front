1a151c587c602780d3857ac6249c715ae15110ff
Andry Jonathan Randrianaly <jonathanrandrianaly@gmail.com>
2025-03-11 12:36:01 +0300

    design liste users et champ de filtre et tri

--- src/app/components/parametre/user-parametre/user-parametre.component.ts
+++ src/app/components/parametre/user-parametre/user-parametre.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {User} from '../../../models/User';
 import {LoaderComponent} from '../../templates/loader/loader.component';
 import {CommonModule} from '@angular/common';
@@ -6,13 +6,14 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableModule} from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
 import {MatDialog} from '@angular/material/dialog';
 import {EmployeeInsertionComponent} from '../user-dialog/employee-insertion/employee-insertion.component';
 import {ApiService} from '../../../services/api/api.service';
 import {Router} from '@angular/router';
+import {MatSort} from '@angular/material/sort';
 
 
 @Component({
@@ -25,7 +26,11 @@ import {Router} from '@angular/router';
 })
 export class UserParametreComponent implements OnInit {
   loader : boolean = false;
-  users : User[] = [];
+  displayedColumns: string[] = ['name', 'id', 'status', 'actions'];
+  searchKey: string = '';
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  users : any[] = [];
   searchCriteria: any = {
     id: '',
     nom: '',
@@ -39,6 +44,8 @@ export class UserParametreComponent implements OnInit {
     sortedColumn : '',
     sortDirection : '',
   };
+  sortedColumn: string = '';
+  sortDirection: 'asc' | 'desc' | '' = '';
   constructor(private dialog: MatDialog, private router: Router, private apiService: ApiService) {
 
   }
@@ -46,11 +53,25 @@ export class UserParametreComponent implements OnInit {
     this.loadUsers();
   }
 
+  sortData(column : string) {
+    if (this.sortedColumn == column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    }
+    else {
+      this.sortedColumn = column;
+      this.sortDirection = 'asc';
+    }
+  }
+
+  isSorted(column: string, direction: 'asc' | 'desc') {
+    return this.sortedColumn === column && this.sortDirection === direction;
+  }
   loadUsers() {
     this.loader = true;
     this.apiService.getAll('api/users').then(
       (response) => {
         this.users = response;
+        console.log(response);
         this.loader = false;
       },
       (error) => {
@@ -58,6 +79,10 @@ export class UserParametreComponent implements OnInit {
         console.error('Erreur lors de loadUsers :', error);
       }
     );
+  }
+
+  applyFilter() {
+
   }
 
   openEmployerDialog() {
