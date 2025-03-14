af7d44e6543cd5c4808c168dbd606607017bee59
Andry Jonathan Randrianaly <jonathanrandrianaly@gmail.com>
2025-03-14 10:48:08 +0300

    schema de demande service interface

--- src/app/components/client/demande-service-client/demande-service-client.component.ts
+++ src/app/components/client/demande-service-client/demande-service-client.component.ts
@@ -1,4 +1,3 @@
-import { Component } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoaderComponent} from '../../templates/loader/loader.component';
 import {MatPaginatorModule} from '@angular/material/paginator';
@@ -9,16 +8,23 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-demande-service-client',
   imports: [ReactiveFormsModule,FormsModule,LoaderComponent,
     MatPaginatorModule,CommonModule,MatButtonModule, MatMenuModule, MatIconModule
     ,MatTableModule,MatChipsModule,
-    MatTooltipModule
+    MatTooltipModule,MatFormFieldModule, MatInputModule, MatDatepickerModule
   ],
   templateUrl: './demande-service-client.component.html',
-  styleUrl: './demande-service-client.component.css'
+  styleUrl: './demande-service-client.component.css',
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DemandeServiceClientComponent {
   loader: boolean = false;
@@ -61,6 +67,12 @@ export class DemandeServiceClientComponent {
       price: "15000 Ariary",
       icon: "more_horiz" // ⋯ Icône pour autres services
     }
+  ];
+
+  disabledDates: Date[] = [
+    new Date(2025, 2, 15),
+    new Date(2025, 2, 21),
+    new Date(2025, 2, 16)
   ];
 
   vehicles: any[] = [
@@ -149,5 +161,42 @@ export class DemandeServiceClientComponent {
     }
   ];
 
+  steps = [
+    { icon: 'work', tooltip: 'Étape 1: Choisir le service' },
+    { icon: 'directions_car', tooltip: 'Étape 2: Choisir le véhicule' },
+    { icon: 'build', tooltip: 'Étape 3: Choisir le type d\'entretien' },
+    { icon: 'check_circle', tooltip: 'Étape 4: Confirmer service' }
+  ];
+
+  get currentStep(): number {
+    if (!this.selectedCategorieEntretien) return 0;
+    if (!this.selectedVehicule) return 1;
+    if (!this.typeEntretiens.filter(entretien => entretien.selected)) return 2;
+    return 3;
+  }
+
+  getStepTitle(): string {
+    if (!this.selectedCategorieEntretien) return 'Veuillez sélectionner le type de service dont vous avez besoin :';
+    if (!this.selectedVehicule) return 'Choisissez votre véhicule :';
+    if (!this.typeEntretiens.filter(entretien => entretien.selected)) return 'Veuillez sélectionner les entretiens ou réparations souhaités :';
+    return 'Confirmez votre panier';
+  }
+
+  dateFilter = (date: Date | null): boolean => {
+    if (!date) return true;
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    if (date < today) {
+      return false;
+    }
+    for (let disabledDate of this.disabledDates) {
+      const disabledDateWithoutTime = new Date(disabledDate.setHours(0, 0, 0, 0));
+      if (date.getTime() === disabledDateWithoutTime.getTime()) {
+        return false;
+      }
+    }
+    return true;
+  };
 
 }
